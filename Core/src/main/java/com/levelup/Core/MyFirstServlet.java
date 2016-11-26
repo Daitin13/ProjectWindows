76bd67db9e41afb99c2d85ff7a72985468ffb376
Daitin13 <Daitin13@gmail.com>
2016-11-26 22:26:49 +0200

    1

--- Core/src/main/java/com/levelup/Core/MyFirstServlet.java
+++ Core/src/main/java/com/levelup/Core/MyFirstServlet.java
@@ -1,4 +1,4 @@
-package com.levelup.webservices;
+package com.levelup.Core;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -6,23 +6,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 /**
  * Created by Daitin on 20.11.2016.
  */
 
 public class MyFirstServlet extends HttpServlet {
     @Override
-    public void init() throws ServletException {
+    public void init() throws  ServletException {
         super.init();
         System.out.println("INIT SERVLET");
     }
-    protected void service (HttpServletRequest reg, HttpServletResponse resp) throws ServletException, IOException{
+
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
         resp.setContentType("application/json;charset=UTF-8");
         resp.setCharacterEncoding("UTF-8");
         out.write(("Servlet work ok").getBytes());
         resp.setStatus(HttpServletResponse.SC_OK);
         out.close();
+    }
+
+    public static void main(String[] args) throws ServletException {
+        MyFirstServlet firstServlet = new MyFirstServlet();
+        firstServlet.init();
+
+
     }
 }
